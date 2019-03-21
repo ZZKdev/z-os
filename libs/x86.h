1cd21322025b03767b5e511b18cc71dcd5129249
zkkdev <2657530327@qq.com>
2019-03-21 23:19:43 +0800

    add inw outw macro

--- libs/x86.h
+++ libs/x86.h
@@ -1,7 +1,20 @@
 #define outb(port, value) \
     __asm__ volatile("outb %%al, %%dx"::"a"(value), "d"(value));
+
+#define outw(port, value) \
+    __asm__ volatile("outw %%ax, %%dx"::"a"(value), "d"(value));
+
+
+
+
 #define inb(port) ({\
     unsigned char _v;\
     __asm__ volatile("inb %%dx, %%al":"=a"(_v):"d"(port));\
+    _v;\
+    })
+
+#define inw(port) ({\
+    unsigned short _v;\
+    __asm volatile("inw %%dx, %%ax":"=a"(_v):"d"(port));\
     _v;\
     })
